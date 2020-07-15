0cbed1ed776b845152cac662e1055eef3a05eec1
qiaochao <qiaochao@csii.com.cn>
2020-07-15 17:26:57 +0800

    des

--- router/root_test.py
+++ router/root_test.py
@@ -2,7 +2,6 @@
 from fastapi import APIRouter, HTTPException, Request, status
 
 def get_base_router()->APIRouter:
-    """Generate a router with the register route."""
     router = APIRouter()
     @router.get("/")
     def read_root(request: Request):
